49c988f33352377df3370a64430b4f5fcfb017d1
psajd <polpilik@gmail.com>
2021-09-19 13:27:59 +0300

    First lab

---
+++ lab1/main.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+void ShowUsage(char *arg) {
+    printf("Usage: %s <option(s)>\n", arg);
+    printf("\t-h, --help\t\tShow this help message\n");
+    printf("\t-c, --bytes\t\tOutput of the file size in bytes\n");
+    printf("\t-l, --lines\t\tOutput the number of rows\n");
+    printf("\t-w, --words\t\tOutput of the number of words\n");
+
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        printf("Error: Invalid number of arguments\n");
+        ShowUsage(argv[0]);
+        return 2;
+    } else {
+
+        char *cmd = argv[1];
+        if (strcmp(cmd, "-h") == 0 || strcmp(cmd, "--help") == 0) {
+            ShowUsage(argv[0]);
+            return 0;
+        }
+
+        FILE *TextFile;
+        TextFile = fopen(argv[2], "r");
+        if (TextFile == NULL) {
+            printf("Error: Error occurred while opening a file");
+            return 1;
+        }
+
+        int chars = 0, lines = 1, words = 0;
+        char TempStr = {0};
+        bool WFlag = true, LFlag = true;
+
+        while (fgets(&TempStr, 2, TextFile)) {
+            if (!((strcmp(&TempStr, " ") == 0) || (strcmp(&TempStr, "\t") == 0) || (strcmp(&TempStr, "\n") == 0)))
+                WFlag = true;
+            else{
+                if (WFlag){
+                    words++;
+                    WFlag = false;
+                }
+                if (strcmp(&TempStr, "\n") == 0)
+                    lines++;
+            }
+            chars++;
+        }
+
+        if (strcmp(&TempStr, "\t") == 0 || strcmp(&TempStr, "\n") == 0 ||
+                strcmp(&TempStr, " ") == 0){
+            words--;
+        }
+
+        if (strcmp(cmd, "-l") == 0 || strcmp(cmd, "--lines") == 0) {
+            printf("%d", lines);
+        } else if (strcmp(cmd, "-c") == 0 || strcmp(cmd, "--bytes") == 0) {
+            printf("%d", chars);
+        } else if (strcmp(cmd, "-w") == 0 || strcmp(cmd, "--words") == 0) {
+            printf("%d", words+(words == 0? 0: 1));
+        } else{
+            printf("Error: This command does not exist\n");
+            ShowUsage(argv[0]);
+            return 3;
+        }
+    }
+    return 0;
+}
