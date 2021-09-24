b6d70dfa064831dcab8a00bbf89e7e5812954b56
Bl4ck8urn <5lickit95loth@gmail.com>
2021-09-24 16:45:17 +0300

    Final version, I hope

--- lab1/wordcount.c
+++ lab1/wordcount.c
@@ -9,10 +9,9 @@
 int main(int argc, char *argv[]) {
     int words = 0, lines = 1, bytes = 0;
     FILE *ptr;
-
-    if (argc == 3 && strcmp(argv[2], " ") != 0) {
-        char previous_blank = 1; //
-        ptr = fopen(argv[2], "r");
+    ptr = fopen(argv[2], "r");
+    if (argc == 3 && ptr != NULL) {
+        char previous_blank = 1;
         char character;
         character = fgetc(ptr);
         while (character != EOF) {
@@ -37,14 +36,14 @@ int main(int argc, char *argv[]) {
             printf("Line amount: %d", lines);
         } else if (strcmp(argv[1], "-w") == 0 || strcmp(argv[1], "--words") == 0) {
             printf("Word amount: %d", words);
-        } else if (strcmp(argv[1], "-b") == 0 || strcmp(argv[1], "--bytes") == 0) {
+        } else if (strcmp(argv[1], "-c") == 0 || strcmp(argv[1], "--bytes") == 0) {
             printf("Byte amount: %d", bytes);
         } else {
-            fprintf(stderr, "Argument input is incorrect");
+            fprintf(stderr, "Invalid argument: '%s'", argv[1]);
             return -1;
         }
     } else {
-        fprintf(stderr, "Either you didn't specify an input file or your argument count is incorrect");
+        fprintf(stderr, "Either you didn't put exactly ONE argument or there was an error opening the file");
         return -1;
     }
 
