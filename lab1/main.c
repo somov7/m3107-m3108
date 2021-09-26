4b244354b0696cdb52a31078a59b879a0249bbe3
anttoinettae <tonyache@mail.ru>
2021-09-26 10:25:40 +0300

    исправлены все ошибки (вроде)

--- lab1/main.c
+++ lab1/main.c
@@ -5,6 +5,7 @@ int main(int argc, char ** argv) {
 
     if (argc != 3) {
         printf("%s", "error (wrong everything)");
+        return -1;
     }
 
     char* cmn;
@@ -23,48 +24,40 @@ int main(int argc, char ** argv) {
     int num_lines = 1;
     int size_f = 0;
     int num_words = 0;
-    int symbol = 5;
-    int flag = 0;
+    int symbol = fgetc(data);
+    int is_letter = 0;
     while (symbol != EOF)
     {
-        symbol = fgetc(data);
         size_f++;
-        if (symbol == '\n' || symbol == '\t' || symbol == '\v' || symbol == '\r'){
+        if (symbol == '\n'){
             num_lines++;
-            flag = 0;
+            is_letter = 0;
+        } else if (symbol == ' '|| symbol == '\t' || symbol == '\v' || symbol == '\r') {
+            is_letter = 0;
         } else {
-            if (symbol == ' ') {
-                flag = 0;
-            } else {
-                if (flag == 0) {
-                    num_words++;
-                }
-                flag = 1;
+            if (is_letter == 0) {
+                num_words++;
             }
+            is_letter = 1;
         }
-
+        symbol = fgetc(data);
     }
+
+
     fclose(data);
     size_f--;
 
-    int cmnok = 0;
-
     if (strcmp(cmn, "-l") == 0 || strcmp(cmn, "--lines") == 0){
         printf("%d",num_lines);
-        cmnok = 1;
-    }
-    if (strcmp(cmn, "-c") == 0 || strcmp(cmn, "--bytes") == 0){
+    } else if (strcmp(cmn, "-c") == 0 || strcmp(cmn, "--bytes") == 0){
         printf("%d",size_f);
-        cmnok = 1;
-    }
-    if (strcmp(cmn, "-w") == 0 || strcmp(cmn, "--words") == 0){
+    } else if (strcmp(cmn, "-w") == 0 || strcmp(cmn, "--words") == 0){
         printf("%d",num_words);
-        cmnok = 1;
+    } else {
+        printf ("%s", "error (wrong command i guess)\n");
+        return -1;
     }
 
-    if (cmnok != 1) {
-        printf("%s", "error (wrong command i guess)");
-    }
 
     return 0;
 }
