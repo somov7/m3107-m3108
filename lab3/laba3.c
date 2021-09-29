41ca566d9b1b7a94f004b0ac79e6146392b9a4fb
exclide <exclide1@gmail.com>
2021-09-29 21:46:47 +0300

    fix logic and style

--- lab3/laba3.c
+++ lab3/laba3.c
@@ -3,12 +3,6 @@
 #include <stdlib.h>
 
 #define LL long long
-LL precount_days[13]; //получить время в секундах марта это сумма дней до марта * 24 * 60 * 60 //LL тут как и далее для автоматических преобразований в long long
-LL days_in_months[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-LL precount_days_leap[13]; //получить время в секундах марта это сумма дней до марта * 24 * 60 * 60 //LL тут как и далее для автоматических преобразований в long long
-LL days_in_months_leap[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-LL years[2022];
-LL years_precount[2022]; //предпосчет дней в году в зависимости високосного года
 
 int isleap(int year)
 {
@@ -20,100 +14,57 @@ int isleap(int year)
     return leap;
 }
 
+const char* months[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+int days[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+LL year_month_to_sec(int year, char* month)
+{
+    LL sec = 0;
+    for (int i = 1; i < year; i++)
+    {
+        sec += isleap(i) ? 366*24*60*60 : 365*24*60*60;
+    }
+    int n = 0;
+    while(strcmp(month, months[n]))
+        n++;
+    for (int i = 0; i < n; i++)
+    {
+        sec += days[i]*24*60*60;
+        if (i == 1 && isleap(year)) sec += 24*60*60;
+    }
+    return sec;
+}
+
 LL tosec(char* str, char* t)
 {
     if (!strcmp(t, "sec")) return atoi(str);
     else if (!strcmp(t, "min")) return 60*atoi(str);
     else if (!strcmp(t, "hour")) return 60*60*atoi(str);
-    else if (!strcmp(t, "day")) return 24*60*60*(atoi(str)-1); //первый день это ноль сек, не будет ситуации что январь 31 > февраля 1
-    else if (!strcmp(t, "year")) return 24*60*60*1LL*years_precount[atoi(str)]; //отсчет От Рождества Христова
-    else
-    {
-        int leap = isleap(atoi(str));
-        if (!strcmp(t, "Jan")) 
-        {
-            if (leap) return precount_days_leap[0]*24*60*60;
-            else return precount_days[0]*24*60*60;
-        }
-        else if (!strcmp(t, "Feb")) 
-        {
-            if (leap) return precount_days_leap[1]*24*60*60;
-            else return precount_days[1]*24*60*60;
-        }
-        else if (!strcmp(t, "Mar")) 
-        {
-            if (leap) return precount_days_leap[2]*24*60*60;
-            else return precount_days[2]*24*60*60;
-        }
-        else if (!strcmp(t, "Apr")) 
-        {
-            if (leap) return precount_days_leap[3]*24*60*60;
-            else return precount_days[3]*24*60*60;
-        }
-        else if (!strcmp(t, "May")) 
-        {
-            if (leap) return precount_days_leap[4]*24*60*60;
-            else return precount_days[4]*24*60*60;
-        }
-        else if (!strcmp(t, "Jun")) 
-        {
-            if (leap) return precount_days_leap[5]*24*60*60;
-            else return precount_days[5]*24*60*60;
-        }
-        else if (!strcmp(t, "Jul")) 
-        {
-            if (leap) return precount_days_leap[6]*24*60*60;
-            else return precount_days[6]*24*60*60;
-        }
-        else if (!strcmp(t, "Aug")) 
-        {
-            if (leap) return precount_days_leap[7]*24*60*60;
-            else return precount_days[7]*24*60*60;
-        }
-        else if (!strcmp(t, "Sep")) 
-        {
-            if (leap) return precount_days_leap[8]*24*60*60;
-            else return precount_days[8]*24*60*60;
-        }
-        else if (!strcmp(t, "Oct")) 
-        {
-            if (leap) return precount_days_leap[9]*24*60*60;
-            else return precount_days[9]*24*60*60;
-        }
-        else if (!strcmp(t, "Nov")) 
-        {
-            if (leap) return precount_days_leap[10]*24*60*60;
-            else return precount_days[10]*24*60*60;
-        }
-        else if (!strcmp(t, "Dec")) 
-        {
-            if (leap) return precount_days_leap[11]*24*60*60;
-            else return precount_days[11]*24*60*60;
-        }
-    }
+    return 24*60*60*(atoi(str)-1); //ничего другого больше не могли передать, кроме дня
 }
 
 int main(int argc, char* argv[])
 {
-    precount_days[0] = 0;
-    precount_days_leap[0] = 0;
-    for (int i = 1; i < 13; i++)
+    if (argc < 3)
     {
-        precount_days[i] = precount_days[i-1] + days_in_months[i-1];
-        precount_days_leap[i] = precount_days_leap[i-1] + days_in_months_leap[i-1];
+        printf("Correct usage: --file=filename --window=timeinsec");
+        return 1;
     }
-    years_precount[0] = 0;
-    years[0] = 0;
-    for (int i = 1; i < 2022; i++)
+    char* filename = NULL;
+    LL window = 0;
+    for (int i = 1; i < argc; i++)
     {
-        int leap = isleap(i);
-        if (leap) years[i] = 366;
-        else years[i] = 365;
+        if (strstr(argv[i], "--file"))
+        {
+            filename = (char*)calloc(strlen(argv[i]) - 6, 1);
+            strcpy(filename, argv[i] + 7);
+        }
+        else if (strstr(argv[i], "--window"))
+        {
+            window = atoll(argv[i]+9);
+        }
     }
-    for (int i = 1; i < 2022; i++)
-        years_precount[i] = years_precount[i-1] + years[i-1];
-
-    FILE* fp = fopen("test.txt", "r"); //название файла
+    FILE* fp = fopen(filename, "r"); //название файла
     if (!fp)
     {
         printf("Can't open file, check name/path\n");
@@ -125,9 +76,6 @@ int main(int argc, char* argv[])
     char** ans = (char**)malloc(ans_size*sizeof(char*)); //выделим сначала массив на 50 строк, если надо будет, сделаем realloc *= 2
     char* buf2[3];
     int error_count = 0;
-    LL window;
-    printf("Enter time window length in seconds.\n");
-    scanf("%lld", &window);
     int lines = 1000; //выделим на 1000, если надо будет, увеличим
     LL* times = malloc(lines*sizeof(LL)); //массив для хранения времени запроса в секундах для подсчета максимального количества запросов в промежутке [a; a+t]
     int n = 0; //реальное количество строк
@@ -164,13 +112,13 @@ int main(int argc, char* argv[])
         split = strtok(NULL, "/: "); //месяц
         char* month = split;
         split = strtok(NULL, "/: "); //год
-        t += tosec(split, "year");
-        t += tosec(split, month);
+        //t += tosec(split, "year");
+        t += year_month_to_sec(atoi(split), month);
+        //t += tosec(split, month);
         c = 0;
         while (split != NULL) //для вычленения времени используем токенайзер strtok, переводим все в секунды, годы и месяцы в файле совпадают
         {
             c++;
-            //printf("c: %d str: %s\n", c, split);
             if (c == 2) t += tosec(split, "hour");
             else if (c == 3) t += tosec(split, "min");
             else if (c == 4) t += tosec(split, "sec");
@@ -182,12 +130,11 @@ int main(int argc, char* argv[])
             lines *= 2;
         }
         times[n++] = t;
-        //printf("time: %d\n", t);
         for (int i = 0; i < 3; i++)
             free(buf2[i]);
 
     }
-    int lp = 0, rp = 0;
+    int lp = 0;
     LL t = 0, mx = 0;
     LL lt = times[0], rt = times[0];
     c = 1;
