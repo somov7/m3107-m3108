69969c5958369ab2d162658a77e5faa4f6401605
Nina Urazova <ninuspinus@bk.ru>
2021-10-16 14:40:42 +0300

    fix bugs

--- lab1/main.c
+++ lab1/main.c
@@ -1,34 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
-size_t count_lines(char *filename) {
-    FILE *fp;
-    size_t cnt = 0;
-    if ((fp = fopen(filename, "r")) == NULL) {
-        return -1;
-    }
-    int current, last = EOF;
+size_t count_lines(FILE *fp) {
+    size_t cnt = 1;
+    int current;
     current = fgetc(fp);
     while (current != EOF) {
-        last = current;
         if (current == '\n') {
             cnt++;
         }
         current = fgetc(fp);
     }
-    if (last != '\n') {
-        cnt++;
-    }
     fclose(fp);
     return cnt;
 }
 
-size_t count_file_size(char *filename) {
-    FILE *fp;
+size_t count_file_size(FILE *fp) {
     size_t cnt = 0;
-    if ((fp = fopen(filename, "r")) == NULL) {
-        return -1;
-    }
     while (getc(fp) != EOF) {
         cnt++;
     }
@@ -36,49 +24,45 @@ size_t count_file_size(char *filename) {
     return cnt;
 }
 
-size_t count_words(char *filename) {
-    FILE *fp;
+size_t count_words(FILE *fp) {
     size_t cnt = 0;
-    if ((fp = fopen(filename, "r")) == NULL) {
-        return -1;
-    }
-    int current, last = EOF;
+    int current;
     current = fgetc(fp);
     while (current != EOF) {
-        last = current;
-        if (current == ' ' || current == '\n') {
-            cnt++;
+        if (current != ' ' && current != '\n') {
             int help = fgetc(fp);
-            while (help == ' ' || help == '\n') {
+            while (help != ' ' && help != '\n' && help != EOF) {
                 help = fgetc(fp);
             }
+            cnt++;
         }
         current = fgetc(fp);
     }
     fclose(fp);
-    if (last != '\n') {
-        cnt++;
-    }
     return cnt;
 }
 
 int main(int argc, char *argv[]) {
     if (argc < 3) {
         printf("Format input: \"WordCount.exe [OPTION] filename\"");
-        return 0;
+        return -1;
     }
-
+    FILE *fp;
+    if ((fp = fopen(argv[2], "r")) == NULL) {
+        return -1;
+    }
     if (strcmp(argv[1], "-l") == 0 || strcmp(argv[1], "--lines") == 0) {
-        printf("file lines: %zu\n", count_lines(argv[2]));
+        printf("file lines: %zu\n", count_lines(fp));
     } else if (strcmp(argv[1], "-c") == 0 || strcmp(argv[1], "--bytes") == 0) {
-        printf("file size: %zu\n", count_file_size(argv[2]));
+        printf("file size: %zu\n", count_file_size(fp));
     } else if (strcmp(argv[1], "-w") == 0 || strcmp(argv[1], "--words") == 0) {
-        printf("file words: %zu\n", count_words(argv[2]));
+        printf("file words: %zu\n", count_words(fp));
     } else {
         printf("Commands:\n"
                "-l / --lines: number of lines\n"
                "-c / --bytes: size of file\n"
                "-w / --words: number of words\n");
+        return -1;
     }
 
     return 0;
