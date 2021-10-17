17dc2a6accc3db210dcbed12201ba6e4da3e92d5
exclide <exclide1@gmail.com>
2021-10-17 17:42:55 +0300

    fix

--- lab3/laba3.c
+++ lab3/laba3.c
@@ -1,66 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define LL long long
-
-int isleap(int year)
-{
-    if (year % 4) return 0;
-    else if (year % 100) return 1;
-    else if (year % 400) return 0;
-    else return 1;
-}
-
 const char* months[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
-int days[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-LL year_month_to_sec(int year, char* month)
+LL tosecs(char* str)
 {
-    LL sec = 0;
-    for (int i = 1; i < year; i++) {
-        sec += isleap(i) ? 366*24*60*60 : 365*24*60*60;
-    }
+    struct tm time = { .tm_year = atoi(str+7) - 1900, .tm_mday = atoi(str), .tm_hour = atoi(str+12), 
+    .tm_min = atoi(str+15), .tm_sec = atoi(str+18), .tm_isdst = -1};
+    char month[4]; 
+    memcpy(month, str+3, 3);
+    month[3] = '\0';
     int n = 0;
-    while(strcmp(month, months[n])) {
+    while(strcmp(month, months[n]))
         n++;
-    }
-    for (int i = 0; i < n; i++) {
-        sec += days[i]*24*60*60;
-        if (i == 1 && isleap(year)) sec += 24*60*60;
-    }
-    return sec;
-}
-
-#pragma pack(push,1)
-typedef struct time {
-    char day[3];
-    char month[4];
-    char year[5];
-    char hour[3];
-    char min[3];
-    char sec[3];
-} time;
-
-void null_terminate(time* T)
-{
-    T->day[2] = '\0';
-    T->month[3] = '\0';
-    T->year[4] = '\0';
-    T->hour[2] = '\0';
-    T->min[2] = '\0';
-    T->sec[2] = '\0';
-}
-
-LL time_to_sec(time* T)
-{
-    LL secs = 0;
-    secs += atoi(T->sec);
-    secs += atoi(T->min)*60;
-    secs += atoi(T->hour)*60*60;
-    secs += (atoi(T->day)-1)*24*60*60;
-    secs += year_month_to_sec(atoi(T->year), T->month);
-    return secs;
+    time.tm_mon = n;
+    return mktime(&time);
 }
 
 int main(int argc, char* argv[])
@@ -97,18 +54,17 @@ int main(int argc, char* argv[])
     LL n = 0; //реальное количество строк
     while (fgets(buf, sizeof(buf), fp) != NULL) //читаем построчно 
     {
-        time T;
+        char str[21];
         int i = 0;
         while (buf[i++] != '[');
-        memcpy(&T, buf+i, 20); //все поля времени фиксированной длины 
-        //если год <= 9999 (работает в пределах разумного)
-        //предполагаем, что NASA логов не было до 1000 года, либо годы форматированы как и остальные поля, например, 0011 год
-        null_terminate(&T);
+        memcpy(str, buf+i, 20);
+        str[20]='\0';
         if (n == lines) {
             lines *= 2;
             times = realloc(times, lines*sizeof(LL));
         }
-        times[n++] = time_to_sec(&T);
+        times[n++] = tosecs(str);
+
         for (i = strlen(buf)-1; buf[i] != ' '; i--);
         if (buf[i-3] == '5') {
             if (error_count == ans_size) {
@@ -127,8 +83,8 @@ int main(int argc, char* argv[])
     for (LL i = 1; i < n; i++) //метод двух указателей, sliding window 
     {
         t += times[i] - times[i-1];
-        c++;
-        while (t > window && lp+1 < n) {
+        c++; //добавляем время в окно t, увеличиаем счетчик на 1
+        while (t > window && lp+1 < n) { //пока окно t больше нужного окна window, убираем время с левого указателя, уменьшаем счетчик
             t -= times[lp+1] - times[lp];
             lp++;
             c--;
