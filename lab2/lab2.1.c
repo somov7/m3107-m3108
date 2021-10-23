16d29fbeaa46d6b89590c49c94e554aed4062015
Vartanov Suren <surengan2016@gmail.com>
2021-10-23 16:26:52 +0300

    Update lab2.1.c

--- lab2/lab2.1.c
+++ lab2/lab2.1.c
@@ -1,4 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS
+define _CRT_SECURE_NO_WARNINGS
 
 #include <locale.h>
 #include <stdio.h>
@@ -6,7 +6,7 @@
 #include <stdint.h>
 #include <string.h>
 #define Findmax(a,b) (((a) > (b)) ? (a) : (b))
-const int cc = 1e9; //основание системы счисления
+const int cc = 1e9;  //radix
 
 typedef struct
 {
@@ -20,22 +20,22 @@ uint1024_t from_uint(unsigned int x)
     if (x >= cc) size = 2;
     else size = 1;
     uint1024_t t;
-    t.int32 = malloc(size * sizeof(int32_t));//выделяем память размером size * sizeof(int32_t)
+    t.int32 = malloc(size * sizeof(int32_t));// size * sizeof(int32_t)
     t.size = size;
     t.int32[0] = x % cc;
     if (size > 1) t.int32[1] = x / cc;
     return t;
 }
 
-void printf_value(uint1024_t x)//вывод
+void printf_value(uint1024_t x)//РІС‹РІРѕРґ
 {
     int size = x.size;
-    printf("%d", !size ? 0 : x.int32[size - 1]); //выводим значимые цифры без 9 нулей
-    for (int index = size - 2; index >= 0; --index) //выводим результат:значимые цифры печатаются в конце массива
+    printf("%d", !size ? 0 : x.int32[size - 1]); //display significant digits without 9 zeros
+    for (int index = size - 2; index >= 0; --index) //output the result: significant digits are printed at the end of the array
         printf("%09d", x.int32[index]);
 }
 
-void scanf_value(uint1024_t* x)//заполнение полного массива
+void scanf_value(uint1024_t* x)//Р·Р°РїРѕР»РЅРµРЅРёРµ РїРѕР»РЅРѕРіРѕ РјР°СЃСЃРёРІР°
 {
     char str[1024];
     scanf("%s", str);
@@ -46,14 +46,14 @@ void scanf_value(uint1024_t* x)//заполнение полного массива
     result.size = size;
     for (int index = len, j = 0; index > 0; index -= 9, j++)
     {
-        str[index] = '\0'; //убираю нули
-        result.int32[j] = (atoi(index >= 9 ? str + index - 9 : str)); //беру 9 цифр с конца, если индекс меньше 9, перехожу на оставшуюся строку
+        str[index] = '\0'; //СѓР±РёСЂР°СЋ РЅСѓР»Рё
+        result.int32[j] = (atoi(index >= 9 ? str + index - 9 : str)); //I take 9 digits from the end, if the index is less than 9, I go to the rest of the line
     }
     *x = result;
 }
 
 
-uint1024_t add_op(uint1024_t x, uint1024_t y)//сложение
+uint1024_t add_op(uint1024_t x, uint1024_t y)//СЃР»РѕР¶РµРЅРёРµ
 {
     uint1024_t result;
     int size = Findmax(x.size, y.size);
@@ -75,7 +75,7 @@ uint1024_t add_op(uint1024_t x, uint1024_t y)//сложение
     return result;
 }
 
-uint1024_t subtr_op(uint1024_t x, uint1024_t y)//вычитание
+uint1024_t subtr_op(uint1024_t x, uint1024_t y)//РІС‹С‡РёС‚Р°РЅРёРµ
 {
     uint1024_t result;
     int size = Findmax(x.size, y.size);
@@ -105,25 +105,26 @@ int main()
     uint1024_t num1, num2;
     char ArrayOfChar[310];
     memset(ArrayOfChar, 0, 310);
-    printf("Введите число\n");
+    printf("Р’РІРµРґРёС‚Рµ С‡РёСЃР»Рѕ\n");
     scanf_value(&num1);
-    printf("Введите число\n");
+    printf("Р’РІРµРґРёС‚Рµ С‡РёСЃР»Рѕ\n");
     scanf_value(&num2);
     fseek(stdin, 0, SEEK_END);
     while (1)
     {
-        printf("Выберете: + - * \n");
+        printf("Р’С‹Р±РµСЂРµС‚Рµ: + - * \n");
         scanf("%c", ArrayOfChar);
        if (!strcmp(ArrayOfChar, "-")) num1 = subtr_op(num1, num2);
         else if (!strcmp(ArrayOfChar, "+")) num1 = add_op(num1, num2);
         else break;
         printf_value(num1);   
         fseek(stdin, 0, SEEK_END);
-        printf("\nВведите число\n");
+        printf("\nР’РІРµРґРёС‚Рµ С‡РёСЃР»Рѕ\n");
         scanf_value(&num2);
         fseek(stdin, 0, SEEK_END);
         return 0;
     }
     }
 
+ 
  
