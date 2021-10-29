55eb324ddadb3a8af4c32e1ed58b61382b318049
Iamnotagenius <Iamnotagenius@github.com>
2021-10-30 01:52:59 +0300

    More features

--- lab3/main.c
+++ lab3/main.c
@@ -28,71 +28,86 @@ typedef struct {
 	
 const char MONTHS[][4] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
 						   "Aug", "Sep", "Oct", "Nov", "Dec"};
-const char *ERROR_MESSAGES[] = {
-	"Internal Server Error",
-	"Not Implemented",
-	"Bad Gateway",
-	"Gateway Timeout",
-	"HTTP Version Not Supported",
-	"Variant Also Negotiates",
-	"Insufficient Storage",
-	"Loop Detected",
-	"Not Extended",
-	"Network Authentication Required"
-};
 
-const char USAGE_MESSAGE[] = "Usage: %s log.txt [-t, --time TIME_WINDOW] [-e, --error-file FILE]\n";
+const char USAGE_MESSAGE[] = "Usage: %s LOG_FILE [-t, --time TIME_WINDOW] [-e, --error-file FILE] [-f, --format ERROR_FORMAT]\n";
+
+const char HELP[] = "NASA Log parser.\nAvailable options are:\n"
+					"    -t, --time         -- Searches time period when the highest amount of requests were handled (Default: 1 minute).\n"
+					"        Time specifier could be provided as suffix to argument (20d, for example).\n" 
+					"        Possible time specifiers:\n"
+					"            m - minute (60 seconds)\n"
+					"            h - hour (60 minutes)\n"
+					"            d - day (24 hours)\n"
+					"            M - month (30 days)\n"
+					"            y - year (12 months)\n"
+					"    -e, --error-file   -- Dumps requests with 5xx status to specified file (Default: don't dump to anything)\n"
+					"    -f, --error-format -- Specify format for dumping errors.\n" 
+					"        Available format specifiers:\n"
+					"            %%d - date of request\n"
+					"            %%a - remote address\n"
+					"            %%r - request\n"
+					"            %%s - status\n"
+					"            %%b - bytes send by request\n"
+					"            %%%% - literal '%'\n";
+
 
 typedef const struct {
 	char short_opt;
 	char *long_opt;
+	bool has_arg;
+	void (*assign)(char *arg, void *pvar);
 } opt_t;
-const int OPTION_LEN = 2;
+
+const int MANDATORY_ARGS = 1;
+const int MAX_FORMAT_LENGTH = 256;
+
+void assign_error_file(char *arg, void *pvar) {
+	if (strcmp(arg, "-") == 0) {
+		*(FILE **)pvar = stdout;
+		return;
+	}
+
+	FILE *f = fopen(arg, "w");
+	if (f == NULL) {
+		fprintf(stderr, "Could not open file '%s'\n", arg);
+		exit(2);
+	}
+	*(FILE **)pvar = f;
+}
+
+void assign_time(char *arg, void *pvar) {
+	int time;
+	char spec;
+	sscanf(arg, "%d%1c", &time, &spec);
+
+	switch (spec) {
+		case 'y':
+			time *= 12;
+		case 'M':
+			time *= 30;
+		case 'd':
+			time *= 24;
+		case 'h':
+			time *= 60;
+		case 'm':
+			time *= 60;
+
+	}
+
+	*(int *)pvar = time;
+}
+
+void assign_str(char *arg, void *pvar) {
+	*(char **)pvar = malloc(MAX_FORMAT_LENGTH);
+	strncpy(*(char **)pvar, arg, MAX_FORMAT_LENGTH);
+}
+
+const int TIME_DEFAULT = 60;
 const opt_t OPTIONS[] = {
-	{ 't', "time" },
-	{ 'e', "error-file" },
+	{ 't', "time", true, assign_time },
+	{ 'e', "error-file", true, assign_error_file },
+	{ 'f', "error-format", true, assign_str }
 };
-
-void assign_option(int opt, char *arg, void *buf[3]) {
-	switch (opt) {
-		case 0:
-			int d = atoi(arg);
-			memcpy(buf[0], &d, sizeof(int));
-			break;
-		case 1:	
-			if (strcmp(arg, "-") == 0) {
-				memcpy(buf[1], &stdout, sizeof(FILE *));
-				break;
-			}
-			FILE *f = fopen(arg, "w");
-			if (f == NULL) {
-				fprintf(stderr, "Could not open file '%s'\n", arg);
-				exit(2);
-			}
-			memcpy(buf[1], &f, sizeof(FILE *));
-			break;
-	}
-}
-
-int handle_short_option(char opt, char *arg, void *buf[3]) {
-	for (int i = 0; i < OPTION_LEN; ++i) {
-		if (opt == OPTIONS[i].short_opt) {
-			assign_option(i, arg, buf);
-			return 0;
-		}
-	}
-	return opt;
-}
-
-int handle_long_option(char *opt, char *arg, void *buf[3]) {
-	for (int i = 0; i < OPTION_LEN; ++i) {
-		if (strcmp(OPTIONS[i].long_opt, opt) == 0) {
-			assign_option(i, arg, buf);
-			return 0;
-		}
-	}
-	return 1;
-}
 
 void invalid_option(char *opt, char *prog) {
 	fprintf(stderr, "Invalid option '%s'\n", opt);
@@ -103,43 +118,50 @@ void invalid_option(char *opt, char *prog) {
 void parse_args(int argc, char **argv, ...) {
 	if (argc < 2) {
 		fprintf(stderr, USAGE_MESSAGE, argv[0]);
+		printf("Type '%s -h' or '%s --help' to get help.\n", argv[0], argv[0]);
 		exit(1);
 	}
-	va_list args;
-	va_start(args, argv);
-	void *buf[3] = {
-		va_arg(args, void *),
-		va_arg(args, void *),
-		va_arg(args, void *)
-	};
-	bool reading_args = true;
-	int arg = 1, opt;
+	if (strcmp(argv[1], "-h") == 0 or strcmp(argv[1], "--help") == 0) {
+		printf(HELP);
+		exit(0);
+	}
+	bool reading_args = true, is_log_file_provided = false;
+	int arg = 1, opts_len = sizeof(OPTIONS)/sizeof(opt_t) + MANDATORY_ARGS,
+			dashes;
+	va_list va_args;
+	void *args[opts_len];
+	
+	va_start(va_args, argv);
+	for (int i = 0; i < opts_len; ++i) {
+		args[i] = va_arg(va_args, void *);
+	}
+	va_end(va_args);
 	while (arg != argc) {
 		if (reading_args and argv[arg][0] == '-') {
-			if (argv[arg][1] == '-') {
-				if (strlen(argv[arg]) == 2) {
-					reading_args = false;
-					arg++;
-					continue;
-				}
-				if (handle_long_option(argv[arg] + 2, argv[arg + 1], buf)) {
-					invalid_option(argv[arg], argv[0]);
-				}
+			if (strcmp(argv[arg], "--") == 0) {
+				reading_args = false;
 				arg++;
+				continue;
 			}
-			else {
-				if (strlen(argv[arg]) == 1) {
-					invalid_option("-", argv[0]);
+			dashes = strspn(argv[arg], "-");
+			for (int i = 0; i < opts_len - MANDATORY_ARGS; ++i) {
+				if (strcmp(argv[arg] + dashes, OPTIONS[i].long_opt) == 0 or 
+								argv[arg][dashes] == OPTIONS[i].short_opt and dashes == 1) {
+					if (OPTIONS[i].has_arg) {
+						if (arg + 1 >= argc) {
+							fprintf(stderr, "Provide an argument to '%s' option\n", argv[arg]);
+							exit(1);
+						}
+						OPTIONS[i].assign(argv[arg + 1], args[i + MANDATORY_ARGS]);
+						arg++;
+					}
+					else {
+						OPTIONS[i].assign(argv[arg], NULL);
+					}
+					goto end_while;
 				}
-				
-				if ((opt = handle_short_option(argv[arg][1], argv[arg + 1], buf))) {
-					char *tmp = calloc(2, 1);
-					*tmp = opt;
-					invalid_option(tmp, argv[0]);
-					free(tmp);
-				}
-				arg++;
 			}
+			invalid_option(argv[arg], argv[0]);
 		}
 		else {
 			FILE *log_file = fopen(argv[arg], "r");
@@ -147,9 +169,16 @@ void parse_args(int argc, char **argv, ...) {
 				fprintf(stderr, "Could not open file '%s'\n", argv[arg]);
 				exit(2);
 			}
-			memcpy(buf[2], &log_file, sizeof(FILE *));
+			*(FILE **)args[0] = log_file;
+			is_log_file_provided = true;
 		}
+end_while:
 		arg++;
+	}
+	if (!is_log_file_provided) {
+		fprintf(stderr, "Log file is not provided.\n");
+		fprintf(stderr, USAGE_MESSAGE, argv[0]);
+		exit(1);
 	}
 }
 
@@ -227,16 +256,78 @@ char *time_to_str(time_t time) {
 	return time_str;
 }
 
+void data_to_str(data_t data, char *format, char *buf) {
+	char *escsymb, *tmp;
+	while (*format) {
+		escsymb = strpbrk(format, "%\\");
+		if (escsymb == NULL) {
+			strcpy(buf, format);
+			break;
+		}
+		strncpy(buf, format, escsymb - format);
+		buf += escsymb - format;
+		if (*escsymb == '%') {
+			switch(*(escsymb + 1)) {
+				case 'a':
+					/* remote_addr */
+					strcpy(buf, data.remote_addr);
+					buf += strlen(data.remote_addr);
+					break;
+				case 'r':
+					/* request */
+					strcpy(buf, data.request);
+					buf += strlen(data.request);
+					break;
+				case 'd':
+					/* date */
+					tmp = time_to_str(data.date);
+					strcpy(buf, tmp);
+					buf += strlen(tmp);
+					free(tmp);
+					break;
+				case 'b':
+					/* bytes_send */
+					buf += sprintf(buf, "%d", data.bytes_send);
+					break;
+				case 's':
+					/* status */
+					buf += sprintf(buf, "%d", data.status);
+					break;
+				case '%':
+					*buf = '%';
+					buf++;
+			}
+		}
+		else if (*escsymb == '\\') {
+			switch(*(escsymb + 1)) {
+				case 't':
+					*buf = '\t';
+					break;
+				case 'n':
+					*buf = '\n';
+					break;
+				case 'r':
+					*buf = '\r';
+					break;
+				case '\\':
+					*buf = '\\';
+					break;
+				
+			}
+			buf++;
+		}
+		format = escsymb + 2;
+	}
+}
+
 int main(int argc, char** argv) {
-	int diff = -1;
+	int diff = 60;
 	FILE *error_file = NULL, *log_file;
-	char *buffer = malloc(BUFFER_SIZE);
+	char *buffer = malloc(BUFFER_SIZE), *error_format = "Error %s: %r";
 	data_t parsed;
 	stack *failed = create_stack(sizeof(data_t));
 	queue *times = create_queue(sizeof(time_t));
-	parse_args(argc, argv, &diff, &error_file, &log_file);
-	if (diff == -1)
-		diff = 60;
+	parse_args(argc, argv, &log_file, &diff, &error_file, &error_format);
 
 	struct {
 		int amount;
@@ -265,16 +356,20 @@ int main(int argc, char** argv) {
 		fscanf(log_file, " ");
 	}
 	
-	free(buffer);
 	
 	printf("There were %d server errors.\n", failed->length);
+	
 	while (pop(failed, &parsed)) {
 		char *time_str = time_to_str(parsed.date);
-		if (error_file != NULL)
-			fprintf(error_file, "[%s] (%s) Error %d: %s\n", time_str, parsed.remote_addr, parsed.status, ERROR_MESSAGES[parsed.status % 100]);
+		if (error_file != NULL) {
+			data_to_str(parsed, error_format, buffer);
+			fprintf(error_file, "%s\n", buffer);
+		}
 		free_data(parsed);
 		free(time_str);
 	}
+
+	free(buffer);
 
 	printf("Most active time window\nfrom: %s\nto: %s\n(%d requests)\n", 
 					time_to_str(time_window.start),
