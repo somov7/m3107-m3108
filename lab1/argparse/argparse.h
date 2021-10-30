e5a1f1738bc960ef6ebc0603c5fe217d1b74676c
Andrei Krivenko <eternaldasein@gmail.com>
2021-10-30 12:06:29 +0300

    refactored

--- lab1/argparse/argparse.h
+++
@@ -1,8 +0,0 @@
-#ifndef ARGPARSE_H
-#define ARGPARSE_H
-
-#include "../structs.h"
-
-struct Args argparse (int argc, char** argv);
-
-#endif
--- lab1/fileparse/fileparse.h
+++
@@ -1,6 +0,0 @@
-#ifndef FILEPARSE_H
-#define FILEPARSE_H
-
-int fileparse (char* filePath, enum argTypes argType);
-
-#endif
