e9a87f8f207ce242621d16dead0d530fa84f6111
Alex Moroz <334830@niuitmo.ru>
2021-10-30 13:39:08 +0300

    fin all, except multi

--- lab2/main.c
+++ lab2/main.c
@@ -3,11 +3,61 @@
 #include <stdlib.h>
 
 const int BASE = 1000000000;
+const int DIGIT_NUMBER = 9;
 
 typedef struct uint1024 {
     int* number;
-    size_t size;
+    int size;
 } uint1024_t;
+
+int max(int a, int b) {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+uint1024_t mult_op(uint1024_t x, uint1024_t y) {
+    int resSize = x.size + y.size;
+    uint1024_t resultMult;
+    resultMult.number = calloc(resSize, sizeof (int));
+    resultMult.size = resSize;
+    for (int i = 0; i < x.size; ++i) {
+        for (int j = 0; j < x.size - i; ++j) {
+            resultMult.number[i + j] += x.number[i] * y.number[j];
+        }
+    }
+    for (int i = 0; i < x.size - 1; ++i) {
+        resultMult.number[i + 1] += resultMult.number[i] / BASE;
+        resultMult.number[i] %= BASE;
+    }
+    return resultMult;
+}
+
+uint1024_t subtr_op(uint1024_t x, uint1024_t y) {
+    uint1024_t resultSubtr;
+    resultSubtr.number = calloc(x.size, sizeof(int));
+    resultSubtr.size = x.size;
+    short underflow = 0;
+
+    for (int i = 0; i < x.size || underflow; ++i) {
+        if (i == x.size) {
+            //break;
+        }
+        resultSubtr.number[i] = x.number[i] - underflow;
+        if (i < y.size) {
+            resultSubtr.number[i] -= y.number[i];
+        }
+        if (resultSubtr.number[i] < 0) {
+            underflow = 1;
+            resultSubtr.number[i] += BASE;
+        } else {
+            underflow = 0;
+        }
+    }
+    return resultSubtr;
+}
 
 uint1024_t from_uint(unsigned int x) {
     uint1024_t y;
@@ -27,36 +77,92 @@ uint1024_t from_uint(unsigned int x) {
 }
 
 uint1024_t add_op(uint1024_t x, uint1024_t y) {
+    int resSize = max(x.size, y.size);
+
     uint1024_t resultAdd;
-    short carry = 0;
-    for (int i = 0; i < 2; ++i) {
-        resultAdd.str[i] = x.str[i] + y.str[i] + carry;
-        carry = 0;
-        if (x.str[i] + y.str[i] > 255) {
-            carry = 1;
+    resultAdd.number = calloc(resSize, sizeof (int));
+    resultAdd.size = resSize;
+    short overflow = 0;
+
+    for (int i = 0; i < resSize || overflow; ++i) {
+        if (i == resSize) {
+            resultAdd.number = realloc(resultAdd.number, (resSize + 1) * sizeof (int));
+            resultAdd.number[resSize] = 0;
+            resultAdd.size += 1;
+        }
+        resultAdd.number[i] += overflow;
+        if (i < x.size) {
+            resultAdd.number[i] += x.number[i];
+        }
+        if (i < y.size) {
+            resultAdd.number[i] += y.number[i];
+        }
+        if (resultAdd.number[i] >= BASE) {
+            overflow = 1;
+            resultAdd.number[i] -= BASE;
+        } else {
+            overflow = 0;
         }
     }
     return resultAdd;
 }
 
-uint1024_t subtr_op(uint1024_t x, uint1024_t y) {
-    uint1024_t resultSubtr = {0};
-    short carry;
-    for (int i = 0; i < 128; ++i) {
-        resultSubtr.str[i] = x.str[i] - y.str[i] - carry;
-        carry = 0;
-        if (x.str[i] - y.str[i] < 0) {
-            carry = 1;
+
+void printf_value(uint1024_t x) {
+    if (x.size == 0) {
+        printf("%d", 0);
+    } else {
+        printf("%d", x.number[x.size - 1]);
+    }
+    for (int i = x.size - 2; i >= 0; --i) {
+        printf("%09d", x.number[i]);
+    }
+}
+
+void scanf_value(uint1024_t* x) {
+    uint1024_t resValue;
+    int resSize;
+    char value[1024];
+    scanf("%s", value);
+    int valueLength = strlen(value);
+    if (valueLength % DIGIT_NUMBER == 0) {
+        resSize = valueLength / DIGIT_NUMBER;
+    } else {
+        resSize = valueLength / DIGIT_NUMBER + 1;
+    }
+    resValue.number = malloc(resSize * sizeof (int));
+    resValue.size = resSize;
+    for (int i = valueLength, j = 0; i > 0; i -= DIGIT_NUMBER, ++j) {
+        value[i] = '\0';
+        if (i >= DIGIT_NUMBER) {
+            resValue.number[j] = atoi(value + (i - 9));
+        } else {
+            resValue.number[j] = atoi(value);
         }
     }
-    return resultSubtr;
+    *x = resValue;
 }
 
 int main() {
-    unsigned int x = 4222222222;
-    unsigned int y = 222922222;
-    uint1024_t x_ = from_uint(x);
-    uint1024_t y_ = from_uint(y);
-    uint1024_t zAdd = add_op(x_, x_);
-    uint1024_t zSubstr = subtr_op(x_, y_);
+    uint1024_t x;
+    uint1024_t y;
+    printf("%s", "Enter uint1024 value n1\n");
+    scanf_value(&x);
+    printf("%s", "Enter uint1024 value n2\n");
+    scanf_value(&y);
+
+    printf("x + y = ");
+    printf_value(add_op(x, y));
+    printf("\nx - y = ");
+    printf_value(subtr_op(x, y));
+    printf("\nx * y = ");
+    printf_value(mult_op(x, y));
+
+    unsigned int a;
+    uint1024_t z;
+    printf("%s", "\nEnter int value\n");
+    scanf("%d", &a);
+    z = from_uint(a);
+    printf("uint a = ");
+    printf_value(z);
 }
