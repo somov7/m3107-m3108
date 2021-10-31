1a82bb93fdadfd43361e569120d71fafe4f882b6
Anton <stady.mail.itmo@gmail.com>
2021-10-31 06:32:35 +0300

    add linked list

---
+++ laba3/list.c
@@ -0,0 +1,5 @@
+//
+// Created by Антон Чемодуров on 31.10.2021.
+//
+
+#include "list.h"
---
+++ laba3/list.h
@@ -0,0 +1,8 @@
+//
+// Created by Антон Чемодуров on 31.10.2021.
+//
+
+#ifndef LABA3_LIST_H
+#define LABA3_LIST_H
+
+#endif //LABA3_LIST_H
