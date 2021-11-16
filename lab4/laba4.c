191f0735d1749dfeb57f985e7e1daa282e1106c9
exclide <exclide1@gmail.com>
2021-11-16 05:21:48 +0300

    lolz

--- lab4/laba4.c
+++ lab4/laba4.c
@@ -2,69 +2,110 @@
 #include <string.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <stdint.h>
 
 #pragma pack(push,1) //чтобы структуры были едиными без паддинга байтов
 typedef struct tagHEADER //структура хедера ID3v2.4
 {
-    char idv3[3]; //ID3 tag
-    char ver[2]; //Version tag.
-    char flag; //flags
-    char size[4]; //size of entire meta in syncsafe int
+    uint8_t idv3[3]; //ID3 tag
+    uint8_t ver[2]; //Version tag.
+    uint8_t flag; //flags
+    uint8_t size[4]; //size of entire meta in syncsafe int
 } ID3HEADER;
 
 typedef struct tagFRAME //(фрейм 10 байтов, но после идет еще один который мол в самой строке для указания юникод/неюникод)
 {
-    char frameid[4]; //frame ID, ALBM, COMM etc
-    char size[4]; //size of tag without frame header
-    char flag[2]; //flags
-    char unicode; //unicode flag right after the frame
+    uint8_t frameid[4]; //frame ID, ALBM, COMM etc
+    uint8_t size[4]; //size of tag without frame header
+    uint8_t flag[2]; //flags
+    uint8_t unicode; //unicode flag right after the frame
 } ID3FRAME;
 #pragma pack(pop)
 
 ID3HEADER header;
 ID3FRAME frame;
 
-int btoi(char ch[4]) //байты в инт
+int btoi(uint8_t ch[4], int is_header) //байты в инт
 {
-    return ch[0] << 21 | ch[1] << 14 | ch[2] << 7 | ch[3] << 0;
+    if (header.ver[0] == 4 || is_header)
+        return (int)ch[0] << 21 | (int)ch[1] << 14 | (int)ch[2] << 7 | (int)ch[3] << 0;
+    return (int)ch[0] << 24 | (int)ch[1] << 16 | (int)ch[2] << 8 | (int)ch[3] << 0;
 }
 
-void itob(int x, char* ch)
+void itob(int x, uint8_t* ch, int is_header)
 {
     int bits = 127;
-    ch[3] = x & bits;
-    ch[2] = (x >> 7) & bits;
-    ch[1] = (x >> 14) & bits;
-    ch[0] = (x >> 21) & bits;
+    if (header.ver[0] == 4 || is_header) {
+        ch[3] = x & bits;
+        ch[2] = (x >> 7) & bits;
+        ch[1] = (x >> 14) & bits;
+        ch[0] = (x >> 21) & bits;
+    }
+    else {
+        ch[3] = x & bits;
+        ch[2] = (x >> 8) & bits;
+        ch[1] = (x >> 16) & bits;
+        ch[0] = (x >> 24) & bits;
+    }
 }
 
-int readidv3(char* filepath, char* prop_name, int set, int* old_frame_size)
+void save_img(uint8_t* buf, uint8_t* img_name, int sz)
 {
-    FILE* f = fopen(filepath, "rb");
+    int i = 0;
+    uint8_t mime[64], pic_type, pic_description[64];
+    strcpy(mime, buf);
+    while (buf[i++] != 0);
+    pic_type = buf[i++];
+    strcpy(pic_description, buf+i);
+    while (buf[i++] != 0);
+    printf("Pic mime: %s pic type: %d pic description: %s\n", mime, pic_type, pic_description);
+    if (strstr(mime, "jpeg")) {
+        sprintf(mime, "%s.jpg", img_name);
+    }
+    else if (strstr(mime, "png")) {
+        sprintf(mime, "%s.png", img_name);
+    }
+    else {
+        sprintf(mime, "%s.hz", img_name);
+    }
+    FILE* img = fopen(mime, "wb");
+    if (!img) {
+        printf("Can't create file");
+        exit(1);
+    }
+    while (i < sz) {
+        fputc(buf[i++], img);
+    }
+}
+
+int readidv3(uint8_t* file_path, uint8_t* prop_name, int set, int* old_frame_size, uint8_t* img_name)
+{
+    FILE* f = fopen(file_path, "rb");
     if (f == NULL) {
         printf("Can't open file");
         exit(1);
     }
     fread(&header, 1, 10, f); //читаем хедер 10 байтов
-    int k = btoi(header.size); //размер всей структуры ID3 включая хедер
+    int k = btoi(header.size, 1); //размер всей структуры ID3 включая хедер
     printf("size struct: %d\n", k);
     int write_pos = 0;
     while (fread(&frame, 1, 11, f)) //читаем фрейм 10 байтов + 1 байт перед строкой юникода
     {
-        if (frame.frameid[0] == 0 || ftell(f) >= k) {
-            write_pos = ftell(f)-11;
-            break;
+        if (frame.frameid[0] == 0 || ftell(f) >= k) { //больше нечего читать, т.к. первый символ в названии ид фрейма не может быть равен нулю
+            write_pos = ftell(f)-11; //либо если нет паддинга нулевыми байтами, то прочитали больше, чем нужно
+            break; //запомним, где остановились читать для перезаписи тега
         }
-        //больше нечего читать, т.к. первый символ в названии ид фрейма не может быть равен нулю
-        //либо если нет паддинга нулевыми байтами, то прочитали больше, чем нужно
-        int sz = btoi(frame.size); //размера значения фрейма (длина строки)
-        char* buf = (char*)malloc(sz); //размер строки включает 1 байт для юникода в начале, поэтому все ок
-        fgets(buf, sz, f); //прочитали структуру фрейма включая 1 байт юникода, теперь читаем значение
-        //прочитает sz-1 байт, в конец добавит нуль терминатор
+        int sz = btoi(frame.size, 0); //размера значения фрейма (длина строки)
+        uint8_t* buf = (uint8_t*)calloc(sz,1);
+        fread(buf, 1, sz-1, f);
+        buf[sz-1] = 0;
+        if (!strcmp(frame.frameid, "APIC") && img_name) {
+            save_img(buf, img_name, sz);
+        }
         if (prop_name == NULL) {
             printf("pos: %-10d id: %-10s size: %-10d value: ", ftell(f), frame.frameid, sz); //выводим все фреймы
             if (frame.unicode) {
-                wprintf(L"%s\n", buf);
+                wprintf(L"%ls\n", buf+2);
             }
             else {
                 printf("%s\n", buf);
@@ -74,7 +115,7 @@ int readidv3(char* filepath, char* prop_name, int set, int* old_frame_size)
             if (set == 0) {
                 printf("id: %-10s value: ", frame.frameid); //вывод одного фрейма
                 if (frame.unicode) {
-                    wprintf(L"%s\n", buf);
+                    wprintf(L"%ls\n", buf+2);
                 }
                 else {
                     printf("%s\n", buf);
@@ -92,29 +133,29 @@ int readidv3(char* filepath, char* prop_name, int set, int* old_frame_size)
     return write_pos;
 }
 
-void updateidv3(char* filepath, char* prop_name, char* prop_val)
+void updateidv3(uint8_t* file_path, uint8_t* prop_name, uint8_t* prop_val)
 {
-    FILE* read = fopen(filepath, "rb");
+    FILE* read = fopen(file_path, "rb");
     FILE* write = fopen("temp", "wb"); //временный для записи
     if (read == NULL || write == NULL) {
         printf("Can't open/create file");
         exit(1);
     }
     int* old_frame_size = calloc(1, sizeof(int));
-    int write_pos = readidv3(filepath, prop_name, 1, old_frame_size);
+    int write_pos = readidv3(file_path, prop_name, 1, old_frame_size, NULL);
     
     int size_diff = strlen(prop_val) - (*old_frame_size-1);
     fread(&header, 1, 10, read); //читаем хедер 10 байтов
-    int k = btoi(header.size); //размер всей структуры ID3 включая хедер
+    int k = btoi(header.size, 1); //размер всей структуры ID3 включая хедер
     k += size_diff; //обновили размер структуры в соответствии с новым фреймом
-    itob(k, header.size);
+    itob(k, header.size, 1);
     fwrite(&header, 1, 10, write); //записали новый хедер
-    char* buf = (char*)malloc(write_pos-10);
+    uint8_t* buf = (uint8_t*)malloc(write_pos-10);
     fread(buf, 1, write_pos-10, read); //прочитали все байты до фрейма
     fwrite(buf, 1, write_pos-10, write); //записали все байты до фрейма, теперь пишем фрейм
     memcpy(frame.frameid, prop_name, 4);
-    char frame_size[4];
-    itob(strlen(prop_val)+1, frame_size);
+    uint8_t frame_size[4];
+    itob(strlen(prop_val)+1, frame_size, 0);
     memcpy(frame.size, frame_size, 4);
     
     fwrite(&frame, 1, 11, write); //записали фрейм
@@ -124,7 +165,7 @@ void updateidv3(char* filepath, char* prop_name, char* prop_val)
     printf("old fr sz: %d\n", *old_frame_size);
     int read_offset = *old_frame_size ? write_pos+10+*old_frame_size : write_pos;
     int read_size = ftell(read) - read_offset;
-    buf = (char*)malloc(read_size);
+    buf = (uint8_t*)malloc(read_size);
     fseek(read, read_offset, SEEK_SET);
     fread(buf, 1, read_size, read);
     fwrite(buf, 1, read_size, write);
@@ -132,43 +173,47 @@ void updateidv3(char* filepath, char* prop_name, char* prop_val)
     
     fclose(read);
     fclose(write);
-    remove(filepath);
-    rename("temp", filepath);
+    remove(file_path);
+    rename("temp", file_path);
 }
 
 
-void getval(char* str, char** target)
+void getval(uint8_t* str, uint8_t** target)
 {
-    char* split = strtok(str,"=");
+    uint8_t* split = strtok(str,"=");
     if (split != NULL)
         split = strtok(NULL, "=");
     if (split == NULL) {
         printf("Wrong format");
         exit(1);
     }
-    *target = (char*)malloc(strlen(split));
+    *target = (uint8_t*)malloc(strlen(split));
     strcpy(*target, split);
 }
 
 
-int main(int argc, char* argv[])
+int main(int argc, uint8_t* argv[])
 {
     setlocale(LC_ALL, "Russian");
+    int a = 0, b = 0, c = 26, d = 135;
+    int t = c << 7 | d << 0;
+    printf("ok: %d\n", t);
 
-    char* filepath = NULL;
-    char* prop_name = NULL;
-    char* prop_value = NULL;
-    int show = 0, get = 0, set = 0;
+    uint8_t* file_path = NULL;
+    uint8_t* prop_name = NULL;
+    uint8_t* prop_value = NULL;
+    uint8_t* img_name = NULL;
+    int show = 0, get = 0, set = 0, extract_img = 0;
 
     if (argc < 3) {
-        printf("Correct usage: --filepath=<path>\n--show\n--set=<propname> --value=<propvalue>\n--get=<propname>");
+        printf("Correct usage: --filepath=<path>\n--show\n--set=<propname> --value=<propvalue>\n--get=<propname>\n--img=<imgname>");
         return 1;
     }
 
     for (int i = 1; i < argc; i++) {
         if (strstr(argv[i], "--filepath")) {
-            getval(argv[i], &filepath);
-            printf("filepath: %s\n", filepath);
+            getval(argv[i], &file_path);
+            printf("file_path: %s\n", file_path);
         }
         else if (strstr(argv[i], "--set")) {
             set = 1;
@@ -181,6 +226,10 @@ int main(int argc, char* argv[])
         else if (strstr(argv[i], "--value")) {
             getval(argv[i], &prop_value);
         }
+        else if (strstr(argv[i], "--img")) {
+            extract_img = 1;
+            getval(argv[i], &img_name);
+        }
         else if (!strcmp(argv[i], "--show")) {
             show = 1;
         }
@@ -191,10 +240,12 @@ int main(int argc, char* argv[])
     }
     
     if (show) 
-        readidv3(filepath, NULL, 0, NULL);
+        readidv3(file_path, NULL, 0, NULL, NULL);
     else if (get)
-        readidv3(filepath, prop_name, 0, NULL);
+        readidv3(file_path, prop_name, 0, NULL, NULL);
+    else if (extract_img)
+        readidv3(file_path, NULL, 0, NULL, img_name);
     else if (set)
-        updateidv3(filepath, prop_name, prop_value);
+        updateidv3(file_path, prop_name, prop_value);
     return 0;
 }
