ffe98d66934cc7377fff962a10f0db28ab0cba50
ignat <polpilik@gmail.com>
2021-11-28 18:38:05 +0300

    Added bmpToPixelsArray function

--- lab5/main.c
+++ lab5/main.c
@@ -37,25 +37,74 @@ int** mallocPixelArray(int height, int width){
     return resArr;
 }
 
+void freePixelArray(int height, int** arr){
+    for (int i = 0; i < height; i++){
+        free(arr[i]);
+    }
+    free(arr);
+}
+
 void rewriteHeadder(FILE *fin, FILE *fout){
     fread(&bmp, 1, sizeof(bitMapFile), fin);
     fwrite(&bmp, 1, sizeof(bitMapFile), fout);
 }
 
-int** bmpToPixelsArray(int height, int width){
+int** bmpToPixelsArray(int height, int width, FILE* fin){
     int **resArr = mallocPixelArray(height, width);
+    fseek(fin, (int)bmp.bfOffs, SEEK_SET);  // перемещаем указатель на начало считываения пикселей
 
-    
+    int realWidth = (width / 8) + ((width % 8 > 0) ? 1 : 0);    // действительная ширина где находятся значащие биты
+    int scanLineSize = realWidth;
+    if (realWidth % 4 != 0){
+        scanLineSize = realWidth + (4 - (realWidth % 4));
+    }
+    char* scanLine = calloc(scanLineSize, sizeof(char));
+
+    for (int y = height - 1; y >= 0; y--){  // записываем снизу вверх, т.к. пиксели считываются с левого нижнего угла
+        fread(scanLine, 1, scanLineSize, fin);  // cчитываем строку с учётом паддинга
+        for (int x = 0; x < width; x++){
+            int curBytePos = x / 8;
+            int curBit = 1 << (7 - x % 8);
+            int pixel = scanLine[curBytePos] & curBit;
+            if (pixel == 0){        // если бит равен 1(белый цвет), то будем помечать его как 1, если равен нулю - нулём
+                resArr[y][x] = 1;
+            }
+            else{
+                resArr[y][x] = 0;
+            }
+        }
+    }
+
+    free(scanLine);
+
+    return resArr;
 }
 
+void printArr(int** arr, int height, int width){
+    for (int y = 0; y < height; y++){
+        for (int x = 0; x < width; x++){
+            printf("%d", arr[y][x]);
+        }
+        printf("\n");
+    }
+}
+
+
+
 int main() {
+
     FILE *fin = fopen("test.bmp", "rb");
     FILE *fout = fopen("Finish.bmp", "wb");
-    rewriteHeadder(fin, fout);
 
-    int **pixelArr;
+    fread(&bmp, 1, sizeof(bitMapFile), fin);
+    printf("height - %d\n  width - %d", bmp.biHeight, bmp.biWidth);
+    printf("\n");
 
+    int **pixelArr = bmpToPixelsArray(bmp.biHeight, bmp.biWidth, fin);
 
+    printArr(pixelArr, bmp.biHeight, bmp.biWidth);
+
+    freePixelArray(bmp.biHeight, pixelArr);
 
     fclose(fin);
     fclose(fout);
