f2dda51923b2ac7b721b642ab5eae62f8c1fffa4
xvsvg <pobedotriples@gmail.com>
2021-12-11 14:30:17 +0300

    final version

--- gol.c
+++ gol.c
@@ -241,7 +241,7 @@ int main(int argc, char **argv)
     else
         dump_frequency = 1;
 
-    for (long long i = 0; (i < max_iterations); i++)
+    for (long long i = 0; ((max_iterations == LONG_LONG_MAX) ? (1) : (i < max_iterations)); i++)
         if ((dump_frequency == 1) ? (1) : (i % dump_frequency == 0))
         {
             universe = newGeneration(&universe, fh, ih);
