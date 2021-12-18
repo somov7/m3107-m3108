025c3edec36ac94b81f85cfc3361485678512d87
Gleb Mikheev <echpochmak83@gmail.com>
2021-12-18 16:07:04 +0300

    arcVer2

--- archiver.c
+++ archiver.c
@@ -45,7 +45,6 @@ void add_file_to_arc(char *file_name, FILE *file_arc) {
         file_header.f_size = ftell(cur_file);
 //         fseek(cur_file, 0, SEEK_SET);
         rewind(cur_file);
-
         printf("adding file %s %llu bytes\n", file_header.f_name, file_header.f_size);
 
         // write header into arc file
@@ -111,9 +110,61 @@ void extract_files(FILE *file_arc){
     }
 }
 
-int main(int argc, char *argv[]) {
+void special_extract(char *file_name, FILE *file_arc){
+    char buffer[BufferSize] = {'\0'};
+    uint64_t number_of_bytes;
+    uint64_t num_of_bytes_to_read;
+    uint64_t need_to_read;
 
-    if (argc < 4 || ((strcmp(argv[3], "--create") != 0) && argc > 4)){
+    FILE *file_out;
+    ArcHeader file_header;
+    uint16_t check = 0;
+    while(1) {
+        // read header (file name and size)
+        number_of_bytes = fread(&file_header.name_len, sizeof(file_header.name_len), 1, file_arc);
+        if (number_of_bytes == 0) break;
+        file_header.f_name = calloc(file_header.name_len + 1, sizeof(char));
+        number_of_bytes = fread(file_header.f_name, sizeof (char), file_header.name_len, file_arc);
+        if (number_of_bytes == 0) break;
+        if (strcmp(file_header.f_name, file_name) == 0){
+            check = 1;
+            printf("here it is! %s\n", file_header.f_name);
+        }
+        else{
+            continue;
+        }
+        number_of_bytes = fread(&file_header.f_size, sizeof (file_header.f_size), 1, file_arc);
+        if (number_of_bytes == 0) break;
+
+        printf("extracting %s %llu bytes\n", file_header.f_name, file_header.f_size);
+        // create new file from arc
+        file_out = fopen(file_header.f_name,"wb");
+        if (file_out == NULL) {
+            printf("can't open file %s to extract\n", file_header.f_name);
+            fseek(file_arc, file_header.f_size, SEEK_CUR);
+            continue;
+        }
+        num_of_bytes_to_read = file_header.f_size;
+        // printf("write %d bytes\n", num_of_bytes_to_read);
+
+        while(num_of_bytes_to_read > 0)
+        {
+            // reading min from BufferSize and bytes_to_read in one loop iteration
+            need_to_read = ( num_of_bytes_to_read >= sizeof(buffer)) ? sizeof(buffer) : num_of_bytes_to_read;
+            number_of_bytes = fread(buffer,sizeof(char),need_to_read,file_arc);
+            if (number_of_bytes == 0 || need_to_read == 0) break;
+            fwrite(buffer, sizeof(char), number_of_bytes, file_out);
+            num_of_bytes_to_read = num_of_bytes_to_read - need_to_read;
+        }
+        free(file_header.f_name);
+        fclose(file_out);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    printf("%d\n", argc);
+    if (argc < 4 || ((strcmp(argv[3], "--create") != 0)
+    && (strcmp(argv[3], "--specExtract") != 0) && argc > 4)){
         printf("wrong number of parameters\n");
         return 1;
     }
@@ -121,9 +172,11 @@ int main(int argc, char *argv[]) {
     uint16_t creation_flag = 0;
     uint16_t extraction_flag = 0;
     uint16_t list_flag = 0;
+    uint16_t spec_extraction_flag = 0;
 
     char *arch_name;
     char *file_list[argc];
+    char *target_file;
     uint32_t number_of_files = 0;
     for (int i = 1; i < argc; i++){
 //        printf("%s\n", argv[i]);
@@ -139,13 +192,18 @@ int main(int argc, char *argv[]) {
         else if (strcmp(argv[i], "--list") == 0){
             list_flag = 1;
         }
+        else if (strcmp(argv[i], "--specExtract") == 0){
+            target_file = argv[++i];
+            spec_extraction_flag = 1;
+        }
         else if (creation_flag == 1){
             file_list[number_of_files] = argv[i];
             number_of_files++;
         }
     }
 
-    printf("flags: %d %d %d\n", creation_flag, extraction_flag, list_flag);
+    printf("flags: %d %d %d %d\n", creation_flag, extraction_flag, list_flag,
+           spec_extraction_flag);
     printf("%s\n", arch_name);
 //    for (int i = 0; i < number_of_files; i++){
 //        printf("%s\n", file_list[i]);
@@ -179,6 +237,15 @@ int main(int argc, char *argv[]) {
             return 1;
         }
         list_files(arc_file);
+        fclose(arc_file);
+    }
+    else if (spec_extraction_flag){
+        arc_file = fopen(arch_name, "rb");
+        if (arc_file == NULL){
+            printf("can't open file %s\n", arch_name);
+            return 1;
+        }
+        special_extract(target_file, arc_file);
         fclose(arc_file);
     }
 
