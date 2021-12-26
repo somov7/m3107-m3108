cd80a6b10828c0682edde0ac06908a03e246ef7d
SvetaYu <svetlanavolga2003@gmail.com>
2021-12-26 21:37:35 +0300

    lab6 extract 1 file

--- lab6.c
+++ lab6.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
- 
-void itoaSize(unsigned int size, unsigned char *result)
+
+void itoaSize(unsigned int size, unsigned char *result) // побитовый сдвиг. перевод из числа в массив символов
 {
     for (int i = 0; i < 4; i++)
     {
         result[i] = (size << (i * 8)) >> 24;
     }
 }
-int atoiSize(unsigned char *size)
+int atoiSize(unsigned char *size) // побитовый сдвиг. перевод из массива символов в число
 {
     int result = 0;
     for (int i = 0; i < 4; i++)
@@ -27,23 +27,23 @@ int myCreate(FILE *fout, char *file)
         return 1;
     }
     fseek(fin, 0, SEEK_END);
-    unsigned int sizeFile = ftell(fin);
-    unsigned int sizeNameFile = strlen(file);
+    unsigned int sizeFile = ftell(fin);       // узнаем размер архивируемого файла
+    unsigned int sizeNameFile = strlen(file); //длина названия файла
     unsigned char size[4];
-    itoaSize(sizeFile, size);
-    fwrite(size, sizeof(char), 4, fout);
-    itoaSize(sizeNameFile, size);
-    fwrite(size, sizeof(char), 4, fout);
-    fwrite(file, sizeof(char), sizeNameFile, fout);
+    itoaSize(sizeFile, size);                       //
+    fwrite(size, sizeof(char), 4, fout);            //
+    itoaSize(sizeNameFile, size);                   // записываем в архив размер файла, размер названия и название архивируемого файла
+    fwrite(size, sizeof(char), 4, fout);            //
+    fwrite(file, sizeof(char), sizeNameFile, fout); //
     fseek(fin, 0, SEEK_SET);
     char buf[4096];
-    for (int i = 0; i < (sizeFile / 4096); i++)
-    {
-        fread(buf, sizeof(char), 4096, fin);
-        fwrite(buf, sizeof(char), 4096, fout);
-    }
-    fread(buf, sizeof(char), sizeFile % 4096, fin);
-    fwrite(buf, sizeof(char), sizeFile % 4096, fout);
+    for (int i = 0; i < (sizeFile / 4096); i++)       //
+    {                                                 //
+        fread(buf, sizeof(char), 4096, fin);          //
+        fwrite(buf, sizeof(char), 4096, fout);        // перезаписываем данные файла в архив
+    }                                                 //
+    fread(buf, sizeof(char), sizeFile % 4096, fin);   //
+    fwrite(buf, sizeof(char), sizeFile % 4096, fout); //
     fclose(fin);
     // remove(file);
     return 0;
@@ -57,7 +57,7 @@ int myList(char *arcFile)
         return 1;
     }
     fseek(fout, 0, SEEK_END);
-    unsigned int arcSize = ftell(fout);
+    unsigned int arcSize = ftell(fout); // узнаем размер архива
     if (arcSize == 0)
     {
         printf("the archive is empty");
@@ -70,7 +70,7 @@ int myList(char *arcFile)
         unsigned int sizeFile = atoiSize(size);
         fread(size, sizeof(char), 4, fout);
         unsigned int sizeNameFile = atoiSize(size);
-        for (int i = 0; i < sizeNameFile; i++)
+        for (int i = 0; i < sizeNameFile; i++) //печатаем названия хранимых архивом файлов
         {
             char symbol = fgetc(fout);
             printf("%c", symbol);
@@ -81,6 +81,47 @@ int myList(char *arcFile)
     return 0;
 }
 int myExtract(char *arcFile)
+{
+    FILE *fin = fopen(arcFile, "rb");
+    if (fin == NULL)
+    {
+        fprintf(stderr, "%s", "ERROR: the file does not exist");
+        return 1;
+    }
+    fseek(fin, 0, SEEK_END);
+    unsigned int arcSize = ftell(fin); // узнаем размер архива
+    fseek(fin, 0, SEEK_SET);
+    while (ftell(fin) < arcSize)
+    {
+        unsigned char size[4];
+        fread(size, sizeof(char), 4, fin);
+        unsigned int sizeFile = atoiSize(size);
+        fread(size, sizeof(char), 4, fin);
+        unsigned int sizeNameFile = atoiSize(size);
+        char filename[sizeNameFile + 1];
+        for (int i = 0; i < sizeNameFile; i++) //считываем название файла
+        {
+            char symbol = fgetc(fin);
+            filename[i] = symbol;
+        }
+        filename[sizeNameFile] = '\0';
+        char *file = filename;
+        FILE *fout = fopen(filename, "wb");
+        char buf[4096];
+        for (int i = 0; i < (sizeFile / 4096); i++)       //
+        {                                                 //
+            fread(buf, sizeof(char), 4096, fin);          //
+            fwrite(buf, sizeof(char), 4096, fout);        // перезаписываем данные файла
+        }                                                 //
+        fread(buf, sizeof(char), sizeFile % 4096, fin);   //
+        fwrite(buf, sizeof(char), sizeFile % 4096, fout); //
+        fclose(fout);
+    }
+    fclose(fin);
+    // remove(arcFile);
+    return 0;
+}
+int myExtractOneFile(char *arcFile, char *name)
 {
     FILE *fin = fopen(arcFile, "rb");
     if (fin == NULL)
@@ -106,16 +147,26 @@ int myExtract(char *arcFile)
         }
         filename[sizeNameFile] = '\0';
         char *file = filename;
-        FILE *fout = fopen(filename, "wb");
+        FILE *fout;
+        if (strcmp(name, file) == 0)
+        {
+            fout = fopen(filename, "wb");
+        }
         char buf[4096];
         for (int i = 0; i < (sizeFile / 4096); i++)
         {
             fread(buf, sizeof(char), 4096, fin);
-            fwrite(buf, sizeof(char), 4096, fout);
+            if (strcmp(name, file) == 0)
+            {
+                fwrite(buf, sizeof(char), 4096, fout);
+            }
         }
         fread(buf, sizeof(char), sizeFile % 4096, fin);
-        fwrite(buf, sizeof(char), sizeFile % 4096, fout);
-        fclose(fout);
+        if (strcmp(name, file) == 0)
+        {
+            fwrite(buf, sizeof(char), sizeFile % 4096, fout);
+            fclose(fout);
+        }
     }
     fclose(fin);
     // remove(arcFile);
@@ -153,7 +204,14 @@ int main(int argc, char **argv)
         }
         else if (strcmp(argv[i], "--extract") == 0)
         {
-            myExtract(arcFile);
+            if (argc == 4)
+            {
+                myExtract(arcFile);
+            }
+            else
+            {
+                myExtractOneFile(arcFile, argv[i + 1]);
+            }
             break;
         }
         else if (strcmp(argv[i], "--list") == 0)
