4d5f575253965e5bde56be08c34c0e3f7102bb18
MagicGiant <88337554+MagicGiant@users.noreply.github.com>
2022-01-14 18:45:13 +0300

    laba 5

---
+++ 5.c
@@ -0,0 +1,310 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <dirent.h>
+#include "str.h"
+char* arr;
+int size = 8;
+int x, y;
+int image_path;
+int bit_span;
+int byte_span;
+
+FILE* fin;
+char* path;
+
+int n=-1, inp=-1, out=-1, dumb_freg=-1;
+
+int get_O(int X, int Y)
+{
+    return (X - 1) + (Y - 1) * x;
+}
+void print()
+{
+    for (int j = y; j >= 1; j--)
+    {
+        for (int i = 1; i <= x; i++)
+        {
+            printf("%i ", arr[get_O(i, j)]);
+        }
+        printf("\n");
+    }
+}
+void check_color()
+{
+    int color = 0;
+    fseek(fin, 59, SEEK_SET);
+    fread(&color, 1, 1, fin);
+    if (color != 0)
+    {
+        for (int j = 1; j <= y; j++)
+            for (int i = 1; i <= x - bit_span; i++)
+                arr[get_O(i, j)] = (arr[get_O(i, j)] - 1) * (-1);
+    }
+}
+void get_10_2(int data)
+{
+    for (int i = size - 1; i >= size - 8; i--)
+    {
+        arr[i] = data % 2;
+        data /= 2;
+    }
+    size += 8;
+    arr = realloc(arr, size);
+}
+int get_2_10(int k, int Y)
+{
+    int data = 0;
+    int j = 1;
+    for (int i = x - k * 8; i > x - 8 - 8 * k; i--)
+    {
+        if (arr[get_O(i, Y)] == 1)
+            data += j;
+        j *= 2;
+    }
+    // printf("%i ", data);
+    return data;
+}
+void scan_file()
+{
+    //Узнаём длину
+    fseek(fin, 18, SEEK_SET);
+    fread(&x, 1, 4, fin);
+
+    //Узнаём высоту
+    fseek(fin, 22, SEEK_SET);
+    fread(&y, 1, 4, fin);
+    
+    //Узнаём количество лишних бит
+    bit_span = (8 - x % 8) % 8;
+    //узнаём настоящую длину строки
+    x += bit_span;
+    //узнаём кл-во промежуточных байтов
+    byte_span = (4 - (x / 8 % 4)) % 4;
+    // printf("\n|%i %i %i|\n",x,  byte_span, bit_span);
+    //Узнаём место начала изображения
+    fseek(fin, 10, SEEK_SET);
+    fread(&image_path, 1, 1, fin);
+    //считываем изображение
+    fseek(fin, image_path, SEEK_SET);
+    for (int j = 0; j < y; j++)
+    {
+        for (int i = 0; i < x / 8; i++)
+        {
+            int data = 0;
+            fread(&data, 1, 1, fin);
+            get_10_2(data);
+        }
+        fseek(fin, byte_span, SEEK_CUR);
+    }
+    //проверяем палитру
+    check_color();
+}
+void print_file()
+{
+    static int it_output=0;
+    it_output++;
+    FILE *fout;
+    fout=fopen(merge(path, merge("//", merge(get_str(it_output), ".bmp"))), "wb");
+    fseek(fin, 0, SEEK_SET);
+    fseek(fout, 0, SEEK_SET);
+
+    char ch;
+    while (!feof(fin))
+    {
+        ch = getc(fin);
+        putc(ch, fout);
+    }
+    fseek(fout, image_path, SEEK_SET);
+    for (int i = 1; i <= y; i++)
+    {
+        for (int k = x / 8 - 1; k >= 0; k--)
+        {
+            putc(get_2_10(k, i), fout);
+        }
+        fseek(fout, byte_span, SEEK_CUR);
+    }
+    fclose(fout);
+}
+bool check(int X, int Y)
+{
+    int lifes = 0;
+    if (X > 1 && arr[get_O(X - 1, Y)] == 1)
+        lifes++;
+    if (X > 1 && Y < y && arr[get_O(X - 1, Y + 1)] == 1)
+        lifes++;
+    if (Y < y && arr[get_O(X, Y + 1)] == 1)
+        lifes++;
+    if (X < x - bit_span && Y < y && arr[get_O(X + 1, Y + 1)] == 1)
+        lifes++;
+    if (X < x - bit_span && arr[get_O(X + 1, Y)] == 1)
+        lifes++;
+    if (X < x - bit_span && Y>1 && arr[get_O(X + 1, Y - 1)] == 1)
+        lifes++;
+    if (Y > 1 && arr[get_O(X, Y - 1)] == 1)
+        lifes++;
+    if (X > 1 && Y > 1 && arr[get_O(X - 1, Y - 1)] == 1)
+        lifes++;
+    if (arr[get_O(X, Y)] == 1 && (lifes == 2 || lifes == 3))
+        return 1;
+    else if (lifes == 3)
+        return 1;
+    else
+        return 0;
+}
+void go_life()
+{
+    if (n==-1)
+        n=1000000;
+    
+    int loading=n/20; 
+    int load_dash=20;
+    if (n<20)
+        loading = 20/n;
+    printf("loading:\n");
+    char a = 177, b = 219;
+    for (int i = 0; i < 20; i++)
+        printf("%c", a);
+    printf("\r");
+    for (int k = 0; k < n; k++)
+    {
+        char* arr_clone = malloc(x * y);
+        for (int i = 0; i < x * y; i++)
+            arr_clone[i] = 0;
+
+        for (int j = 1; j <= y; j++)
+        {
+            for (int i = 1; i <= x; i++)
+            {
+                arr_clone[get_O(i, j)] = check(i, j);
+            }
+        }
+        
+
+        bool uniformity=true;
+        if (n==1000000)
+        {
+            for (int i = 0; i < x * y; i++)
+                if (arr[i] != arr_clone[i])
+                    {
+                        uniformity=false;
+                        break;
+                    }
+            if (uniformity)
+                break;
+        }
+
+        for (int i = 0; i < x * y; i++)
+            arr[i] = arr_clone[i];
+        
+        if(k%loading==0 && load_dash>1 && n>=20)
+        {
+            printf("%c", b);
+            load_dash--;
+        }
+        if(n<20)
+        {
+            for (int i=0; i<loading; i++)
+            {
+                if (load_dash<=1)
+                    break;
+                printf("%c", b);
+                load_dash--;
+            }
+
+        }
+        free(arr_clone);
+        if(dumb_freg!=-1 && k%dumb_freg==0)
+            print_file();
+    }
+    for (int i=0; i<load_dash-1; i++)
+        printf("%c", b);
+    printf("%c", b);
+    printf("\n");
+    check_color();
+}
+bool check_args()
+{
+    if(inp==-1)
+    {
+        printf("NO INPUT FILE ENTERED\n--ERROR--\n____________________\n");
+        return 1;
+    }
+    if(out==-1)
+    {
+        printf("NO OUTPUT DIRECTORY\n--ERROR--\n____________________\n");
+        return 1;
+    }
+    if(n==-1)
+        printf("* WARNING! NO MAX ITERATOR\n");
+    if(dumb_freg==-1)
+        printf("* WARNING! NO DUMB FREG. IT WILL BE 1\n");
+    return 0;
+}
+int main(int argc, char** argv)
+{
+    int i=1;
+    bool error=false;
+    printf("____________________\n");
+    while (i<argc)
+    {
+        if (strcmp(argv[i], "--input")==0)
+        {
+            fin = fopen(argv[i+1], "rb");
+            if (fin)
+                printf("INPUT FILE: %s\n", argv[i+1]);
+            else
+            {
+                error=true;
+                printf("NOT FOUND FILE\n");
+            }
+            inp=i+1;
+        }
+        if (strcmp(argv[i], "--output")==0)
+        {
+            path=malloc(strlen(argv[i+1])+1);
+            path = argv[i+1];
+            DIR *di = opendir (argv[i+1]);
+            if (di)
+                printf("OUTPUT DIRECTORY: %s\n", argv[i+1]);
+            else
+            {
+                error=true;
+                printf("NOT FOUND DIRECTORY\n");
+            }
+            closedir (di);
+            out=i+1;
+        }
+        if (strcmp(argv[i], "--max_iter")==0)
+        {
+            n=0;
+            for (int j=0; j<strlen(argv[i+1]); j++)
+                n=n*10+argv[i+1][j]-'0';
+            printf("MAX ITERATOR: %i\n", n);
+        }
+        if (strcmp(argv[i], "--dumb_freg")==0)
+        {
+            dumb_freg=0;
+            for (int j=0; j<strlen(argv[i+1]); j++)
+                dumb_freg=dumb_freg*10+argv[i+1][j]-'0';
+            printf("DUMB FREG: %i\n", dumb_freg);
+        }
+        i+=2;
+    }
+    // 5.exe --input 12.bmp --output D:\\testing --max_inter 50 --dumb_freg 10
+    if (error)
+    {
+        printf("--ERROR--\n____________________\n");
+        return 0;
+    }
+    arr=malloc(size);
+    if(check_args())
+        return 0;
+    scan_file();
+    go_life(n);
+    if (dumb_freg==-1)
+        print_file();
+    printf("--NICE--\n____________________\n");
+    return 0;
+}
--- str.h
+++ str.h
@@ -1,11 +1,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include <stdbool.h>
 char *merge (char *arr1, char *arr2)
 {
     int size_str=strlen(arr1)+strlen(arr2);
-    char *str=(char*)malloc(size_str+1);
+    char *str=malloc(size_str+1);
     for (int i=0; i<size_str; i++)
         if (i<strlen(arr1))
             str[i]=arr1[i];
@@ -23,39 +22,13 @@ char *get_str (int data)
         size_str++;
         data_clone/=10;
     }
-    if (data==0)
-        size_str++;
-    bool znak=0;
-    if(data<0)
-        znak=1;
-    size_str+=znak;
-
-    char *str=(char*)malloc(size_str+1);
+    char *str=malloc(size_str+1);
     int j=1;
-    for (int i=size_str-1; i>=znak; i--)
+    for (int i=size_str-1; i>=0; i--)
     {
-        int ch=data%10;
-        if (ch<0)
-            ch*=-1;
-        str[i]=ch+'0';
+        str[i]=data%10+'0';
         data/=10;
     }
-    if (znak==1)
-        str[0]='-';
     str[size_str]=0;
     return str;
-}
-int get_int (char *str)
-{
-    int data=0;
-    bool k;
-    if (str[0]=='-')
-        k=1;
-    else
-        k=0;
-    for(int i=k; i<strlen(str); i++)
-        data=data*10+str[i]-'0';
-    if (k==1)
-        data*=-1;
-    return data;
 }
