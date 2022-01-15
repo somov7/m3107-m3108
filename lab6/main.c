0f012ef026b4f6ff726701e035915dce3f1af054
Borisov Ignat <90337398+Telezhka-the-First@users.noreply.github.com>
2022-01-15 15:42:30 +0300

    Add files via upload

--- lab6/main.c
+++ lab6/main.c
@@ -41,7 +41,7 @@ int main(int argc, char *argv[]) {
             fseek(current, 0, SEEK_END);
             long amount = ftell(current);
             char length[32];
-            sprintf(length, "%ld", amount);
+            sprintf(length, "%09ld", amount);
             fputs(length, archive);
             fputc( '|', archive);
             fclose(current);
@@ -58,18 +58,20 @@ int main(int argc, char *argv[]) {
                 fputc(now, archive);
             }
         }
+        fclose(archive);
+        return 0;
+    }
+
+    FILE *archive = fopen(filename, "rb");
+    if (!archive) {
+        printf("Error: no file");
+        return -1;
     }
 
     if (argc == 4) {
-        FILE *archive = fopen(filename, "rb");
-        if (!archive) {
-            printf("Error: no file");
-            return -1;
-        }
-
         int count = 0;
         for (int i = 0; i < 4; i++) {
-            char temp;
+            int temp;
             temp = fgetc(archive);
             count += (temp - '0') * pow(10, 3 - i);
         }
@@ -84,26 +86,52 @@ int main(int argc, char *argv[]) {
                     if (counting % 2 != 0) printf("\n");
                 } else if (counting % 2 != 0) printf("%c", currentChar);
             }
-        } else if (strcmp(command, "--extract") == 0) {
+            fclose(archive);
+            return 0;
+        }
+        if (strcmp(command, "--extract") == 0) {
             fgetc(archive); // getting '|'
-            long file_lengths[count];
-            char **file_names;
-            int counting = 0;
-            while (counting < count * 2 + 1) {
-                char currentChar = fgetc(archive);
-                if (currentChar == '|') {
-                    counting++;
-                    if (counting % 2 != 0) printf("\n");
-                } else if (counting % 2 != 0) printf("%c", currentChar);
+            FILE *input = fopen(filename, "rb");
+            FILE *output;
+
+            while (1) {
+                int cur_symb = fgetc(input);
+                if (cur_symb == '\n') break;
             }
 
-        } else {
-            printf("Error: wrong command");
-            return -1;
+            char new_filename[256];
+
+            for (int i = 0; i < count; i++) {
+                int k;
+                for (k = 0; k < 256; k++) {
+                    char now = fgetc(archive);
+                    if (now == '|') break;
+                    new_filename[k] = now;
+                }
+                new_filename[k] = '\0';
+
+                output = fopen(new_filename, "wb");
+
+                int cur_file_length = 0;
+                for (int j = 0; j < 9; j++) {
+                    cur_file_length += (fgetc(archive) - '0') * pow(10, 8 - j);
+                }
+                printf("%s   %ld\n", new_filename, cur_file_length);
+                for (int j = 0; j < cur_file_length; j++) {
+                    fputc(fgetc(input), output);
+                }
+                fgetc(archive);
+
+                fclose(output);
+            }
+
+            free(new_filename);
+            fclose(input);
+            fclose(archive);
+            return 0;
         }
     }
 
-
-
-    return 0;
+    printf("Error: wrong command");
+    return -1;
 }
