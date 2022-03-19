f00cb7b84c583a01ca8dfbeca83c96cac6752d3e
Borisov Ignat <90337398+Telezhka-the-First@users.noreply.github.com>
2022-03-19 14:25:49 +0300

    Add files via upload

--- lab3/main.c
+++ lab3/main.c
@@ -61,7 +61,7 @@ int main()
 
         sscanf(time_of_request,"%d/%3s/%d:%d:%d:%d", &time.tm_mday, month, &time.tm_year, &time.tm_hour, &time.tm_min, &time.tm_sec);
 
-        int cur_month = 0;
+        short cur_month;
 
         for (cur_month = 0; cur_month < 12; cur_month++) {
             if (!strcmp(months[cur_month], month)) break;
@@ -75,23 +75,23 @@ int main()
         line++;
     }
 
-    int difference = 0, current_length = 1, maximum = 0, j = 1, left_bound_final, right_bound_final;
+    int difference = 0, current_length = 1, maximum = 0, position = 1, left_bound_final, right_bound_final;
 
-    for (int i = 1; i < line_count; i++) {
+    for (int cur_line = 1; cur_line < line_count; cur_line++) {
 
         current_length++;
-        difference += line_time[i] - line_time[i - 1];
+        difference += line_time[cur_line] - line_time[cur_line - 1];
 
-        while ((difference > window) && (j + 1 < line_count)) {
-            difference -= (line_time[j] - line_time[j - 1]);
-            j++;
+        while ((difference > window) && (position + 1 < line_count)) {
+            difference -= (line_time[position] - line_time[position - 1]);
+            position++;
             current_length--;
         }
 
         if (current_length > maximum) {
             maximum = current_length;
-            left_bound_final = j;
-            right_bound_final = i;
+            left_bound_final = position;
+            right_bound_final = cur_line;
         }
     }
 
@@ -103,7 +103,7 @@ int main()
     time_t left_bound_final_ll = line_time[left_bound_final];
     time_start = localtime(&left_bound_final_ll);
 
-    printf("\nMaximum quantity of requests in %ld second(s) window: %d\nFrom %s", window, maximum, asctime(time_start));
+    printf("\nMaximum quantity of requests in %lld second(s) window: %d\nFrom %s", window, maximum, asctime(time_start));
 
     time_t right_bound_final_ll = line_time[right_bound_final];
     time_end = localtime(&right_bound_final_ll);
